e6d0a4821e796b5c0cea472844faac1c2adfa01c
rojung <roland.jung@aau.at>
2020-04-15 21:05:00 +0200

    [TASK] PlotterModel.periodic_call(): should be called from tk.Topview

--- PlotGUI/PlotterModel.py
+++ PlotGUI/PlotterModel.py
@@ -1,19 +1,28 @@
 from logger.DataPlotter import DataPlotter
 
-
 class PlotterModel():
-    logger = None
+    plotter = None
     view = None
+
     def __init__(self, view):
         self.view = view
 
-    def create_plotter(self, filename, format_str, header, flush_data):
-        self.logger = DataPlotter(filename=filename, format_str=format_str,
-                                 header=header,flush_data=flush_data);
-        return self.logger
+    def create(self, title, format_str, use_timestamp, legend, max_samples):
+        self.plotter = DataPlotter(title=title,
+                                   format_str=format_str,
+                                   use_timestamp=use_timestamp,
+                                   legend=legend,
+                                   max_samples=max_samples)
+        return self.plotter
 
-    def close_logger(self):
-        self.logger.stop()
+    def close(self):
+        del self.plotter
+        self.plotter = None
+        pass
 
-    def get_logger(self):
-        return self.logger
+    def get(self):
+        return self.plotter
+
+    def periodic_call(self):
+        if self.plotter is not None:
+            self.plotter.plot_figure()
